ceff2c8a98ad21754c5eaf42456ad70ee1fac4d4
baoanjie <baoanj@126.com>
2022-08-26 10:56:58 +0800

    mine

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,39 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue'
-import { Mark } from './interface'
-import MarkList from '@/components/MarkList.vue'
+import { ref } from 'vue'
 
-const list = ref<Mark[]>([])
-const sections = [
-  { title: '导航', type: 'nav' },
-  { title: '常看', type: 'top' },
-  { title: '要看', type: 'ned' },
-  { title: '用得着', type: 'use' },
-  { title: '吃灰', type: 'nor' }
-]
-const isInExt = !!chrome.storage
+const mine = ref('')
 
-async function getList() {
+function getPwd() {
   if (chrome.storage) {
-    chrome.storage.local.get('vite', function (res: any) {
-      list.value = res.vite || []
+    chrome.storage.local.get('mine', function (res: any) {
+      mine.value = res.mine
     })
-  } else {
-    const res = await fetch('https://vite.baj.workers.dev/get')
-    const text = await res.text()
-    list.value = JSON.parse(text || '[]')
   }
 }
 
-async function sync() {
-  const res = await fetch('https://vite.baj.workers.dev/get')
-  const text = await res.text()
-  chrome.storage.local.set({ vite: JSON.parse(text || '[]') }, function () {
-    getList()
-  })
+function setPwd() {
+  if (chrome.storage) {
+    chrome.storage.local.set({ mine: mine.value })
+  }
 }
 
-getList()
+getPwd()
 </script>
 
 <template>
-  <button v-if="isInExt" class="sync" @click="sync">同步</button>
-  <div v-for="sec in sections" :key="sec.type">
-    <h2>{{ sec.title }}</h2>
-    <MarkList
-      :list="list.filter(item => item.type === sec.type)"
-      @refresh="getList"
-    />
-  </div>
+  <textarea v-model="mine" rows="20" @input="setPwd"></textarea>
 </template>
 
 <style>
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
-  width: 700px;
+  width: 300px;
   margin: 0 auto;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -58,7 +42,11 @@ getList()
 </style>
 
 <style lang="scss" scoped>
-.sync {
-  float: right;
+textarea {
+  width: 100%;
+  padding: 10px;
+  outline: none;
+  border: none;
+  resize: none;
 }
 </style>
--- src/interface.ts
+++
@@ -1,6 +0,0 @@
-export interface Mark {
-  title: string
-  url: string
-  ico: string
-  type: string
-}
